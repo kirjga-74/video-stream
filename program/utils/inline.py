fd2ce94005433111e8459c3773f72259dabe362a
kirjga-74 <84054027+kirjga-74@users.noreply.github.com>
2022-02-14 01:26:05 +0530

    Update inline.py

--- program/utils/inline.py
+++ program/utils/inline.py
@@ -21,6 +21,47 @@ def stream_markup(user_id):
 def menu_markup(user_id):
   buttons = [
     [
+#		python code
+#		script_name: Game_Intro.py
+#
+#		author: 101Computing
+#		description: Background music for intro 
+#
+
+from earsketch import *
+
+init()
+setTempo(120)
+
+clip1 = EIGHT_BIT_ATARI_SFX_004
+clip2 = EIGHT_BIT_ATARI_LEAD_011
+clip3 = EIGHT_BIT_ATARI_LEAD_010
+
+pointA = 1.75
+repeat = 3
+pointD = pointA + repeat 
+
+#fitMedia(Clip,Track,StartMeasure,EndMeasure)
+fitMedia(clip1,1,1,2)
+for i in range(0,repeat):
+  fitMedia(clip2,2,pointA + i,pointA + i + 1)
+
+fitMedia(clip3,3,pointA,pointD + 1)
+
+#setEffect(Track,effect,parmeter,value)
+setEffect(3, VOLUME, GAIN, -10)
+
+#setEffect(Track,effect,parmeter,value,start measure,value,end measure)
+#Fade in
+setEffect(1, VOLUME, GAIN, -40, 1, 0, 1.75)
+#Delay Effect
+setEffect(1, DELAY, DELAY_TIME, 250)
+setEffect(2, DELAY, DELAY_TIME, 250)
+#Fade Out
+setEffect(3, VOLUME, GAIN, -10, pointD, -60, pointD+1)
+
+finish() 
+
       InlineKeyboardButton(text="⏹", callback_data=f'set_stop | {user_id}'),
       InlineKeyboardButton(text="⏸", callback_data=f'set_pause | {user_id}'),
       InlineKeyboardButton(text="▶️", callback_data=f'set_resume | {user_id}'),
